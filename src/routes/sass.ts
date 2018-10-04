01e2c2c0b0569a272386d0004d1cb780330a4dea
Доможиров Михаил <michael@domozhirov.ru>
2018-10-04 13:32:48 +0500

    Исправлена ошибка компиляции scss и добавлен обработчик compass в виде пустышки чтоб не поломать старые проекты

--- src/routes/sass.ts
+++ src/routes/sass.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 import {Context} from "koa";
 import * as Router from "koa-router";
-import {Options, renderSync, Result} from "node-sass";
+import {Options, render, Result} from "node-sass";
 import * as path from "path";
 import cache from "../libs/cache";
 import Settings from '../libs/settings';
@@ -28,17 +28,21 @@ export default router.all(/.+\.scss?(\.css)(?:\?.*)?/, async (ctx: Context, next
     }
 }).routes();
 
-function compileSass(url: string, options: Options): Result {
-    options.importer = (file, prev, done) => {
-        if (file.indexOf("global:") === 0) {
-            file = path.join("/g/", file.slice(7));
-            cache(file).then((file) => {
-                done({file: file});
-            }).catch((err) => {
-                done(err);
-            });
-        } else {
-            if (file.indexOf("local:") === 0) {
+async function compileSass(url: string, options: Options): Promise<any> {
+    return new Promise((resolve, reject) => {
+        options.importer = (file, prev, done) => {
+            if (file.indexOf("compass") === 0) {
+                done({
+                    contents: ''
+                })
+            } else if (file.indexOf("global:") === 0) {
+                file = path.join("/g/", file.slice(7));
+                cache(file).then((file) => {
+                    done({file: file});
+                }).catch((err) => {
+                    done(err);
+                });
+            } else if (file.indexOf("local:") === 0) {
                 url  = url.slice(0, url.indexOf("/images/"));
                 file = file.slice(6);
 
@@ -46,8 +50,14 @@ function compileSass(url: string, options: Options): Result {
             } else {
                 done({file: path.join(dir, file)});
             }
-        }
-    };
+        };
 
-    return renderSync(options);
+        render(options, (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        })
+    });
 }
