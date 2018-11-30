7adb9a84d473ec5cb27352d9e2be9f757b15fec3
Domozhirov Mikhail <michael@domozhirov.ru>
2018-11-30 11:27:34 +0500

    Ошибка в пути к настройкам

--- src/bootstrap/config.ts
+++ src/bootstrap/config.ts
@@ -2,7 +2,7 @@ import Config from '../core/config';
 import {homedir} from 'os';
 import {join} from 'path';
 
-const configPath = `${homedir}/.wenv3/settings.json`;
+const configPath = `${homedir()}/.wenv3/settings.json`;
 const configDir = join(__dirname, `../conf.d/`);
 const config = new Config(configPath, configDir);
 
